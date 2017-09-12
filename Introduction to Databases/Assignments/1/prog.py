2d11ea9bea28a280971f2517524fa321830429aa
AMaini503 <aayush.maini@gmail.com>
2017-09-12 14:34:57 -0400

    Completed implementation of insert claue

--- Introduction to Databases/Assignments/1/prog.py
+++ Introduction to Databases/Assignments/1/prog.py
@@ -2,6 +2,7 @@ import re
 from prettytable import PrettyTable
 import csv
 from StringIO import StringIO
+from bisect import bisect
 def GetQueryObj(raw_query_text):
 	raw_query_text = raw_query_text.strip()
 
@@ -17,7 +18,6 @@ def GetQueryObj(raw_query_text):
 	return (q_type, obj)
 
 
-	
 def start(): 
 
 	def ParseQuery():
@@ -47,16 +47,19 @@ def start():
 			if q_type == "find":
 				find_query_obj = FindQuery(table_obj, aux_info)
 				find_query_obj.execute()
+			#would always be an insert because invalid query already handled in parse
 			else:
-				insert_query_obj = None
+				insert_query_obj = InsertQuery(table_obj, aux_info)
+				insert_query_obj.execute()
 		
 
 	#########################################################
 	customers = Table("customers", "customers.csv")
 	orders = Table("orders", "orders.csv")
 
-	customers.buildIndex("CustomerID")
-	customers.buildIndex("ContactTitle")
+	# customers.buildIndex("CustomerID")
+	# customers.buildIndex("ContactTitle")
+	customers.setPrimaryKeyAs("CustomerID")
 	# orders.buildIndex("\"OrderID\"")
 
 	#########################################################
@@ -73,10 +76,58 @@ def start():
 			HandleQuery(raw_query_text)
 
 class InsertQuery:
-	def __init__(self, raw_query):
-		pass
-	def execute():
-		pass
+	def __init__(self, table_obj, aux_info):
+
+		#needed because this object holds the in-memory image of the data
+		#update the rows of this object
+		self.table_obj = table_obj
+		self.aux_info = aux_info
+	
+	def execute(self):
+
+		#remove the round brackets from the end
+		csv_val_string = self.aux_info[1:-1]
+
+		csv_array = ParseCSVString2Array(csv_val_string)
+		num_values_in_query = len(csv_array)
+		num_cols_in_table = len(self.table_obj.getAllColNames())
+
+		#1st check is to validate whether user entered a value for all the columns
+		if num_values_in_query < num_cols_in_table:
+			print "FAILED: #values < #columns"
+			return
+		else:
+			primary_key_col_name = self.table_obj.getPrimaryKeyCol()
+			if primary_key_col_name != None:
+
+				#add a check to verify that user has entered a value for primary key 
+				primary_key_col_index = self.table_obj.getAllColNames().index(primary_key_col_name)
+
+				if csv_array[primary_key_col_index] == "": 
+					print "FAILED: Primary Key attribute must have a non-empty value"
+					return
+
+				all_rows_in_table = self.table_obj.getAllRows()
+				all_values_in_primary_key_col = map(lambda row_string: ParseCSVString2Array(row_string)[primary_key_col_index], all_rows_in_table)
+
+				if csv_array[primary_key_col_index] in all_values_in_primary_key_col:
+					print "A record with the same value for primary key exists"
+					table = PrettyTable(self.table_obj.getAllColNames())
+					exisiting_row_index = all_values_in_primary_key_col.index(csv_array[primary_key_col_index])
+					table.add_row(ParseCSVString2Array(all_rows_in_table[exisiting_row_index]))
+					print table
+					return
+				else:
+					#insert the record at appropriate position
+					insertion_index_for_new_record = bisect(all_values_in_primary_key_col, csv_array[primary_key_col_index])
+					
+					#inserting row in the in-memory image of records
+					#table takes care of the updating the index
+					self.table_obj.insertRowAt(csv_val_string, insertion_index_for_new_record)
+					print "Row added successfully"
+
+					#start the actual write to the file
+
 class FindQuery:
 	
 	def __init__(self, table_obj, aux_info):
@@ -201,39 +252,83 @@ class FindQuery:
 				for row_string in records_in_final_result:
 					table.add_row(ParseCSVString2Array(row_string))
 				print table
+		else: 
+			#no records to process
+			print "0 records found"
 		
 
-		
+#Index here is a map from a col_value to the set of row indices that have the "col_value" under colname attribute	
 
 class Index: 
-	def __init__(self, colname, hashed_array):
+	def __init__(self, colname, hashed_array, table_obj):
 		self.colname = colname
 		self.hashed_array = hashed_array
+		self.table_obj = table_obj
 	def getArray(self):
 		return self.hashed_array
 	def getColName(self):
 		return self.colname
 
+	def updateIndexWith(self, hash_key_col_value, hash_val_row_index, _indices_that_need_increment):
+		if hash_key_col_value not in self.hashed_array:
+			self.hashed_array[hash_key_col_value] = set()
+		
+		#row_index for those records which are after this record needs to be incremented by 1
+		all_hash_items = self.hashed_array.iteritems()
+
+		for (_col_val, _row_index_set) in all_hash_items:
+
+			_row_index_list = list(_row_index_set)
+
+			#increment the row_index only if it belongs to the list passed as paramter to the function
+			_updated_row_index_list = map(lambda _row_index: _row_index + 1 if _row_index in _indices_that_need_increment else _row_index, _row_index_list)
+
+			self.hashed_array[_col_val] = set(_updated_row_index_list)
+
+		self.hashed_array[hash_key_col_value].add(hash_val_row_index)
+
+#removes the double quotes from the values in a query
 def ParseCSVString2Array(string):
 	f = StringIO(string)
 
 	reader = csv.reader(f, delimiter=",", skipinitialspace=True)
 	for row in reader:
 		return row
+
+	#comes here only if empty string
+	return []
+
+#removal is necessary because keys in hash and values in a query are processed without any quotes
 def removeDQuotes(string):
 	string = string.replace("\"", "")
 	return string
 class Table: 
 	def __init__(self, tname, filename):
 		self.name = tname
+		#contains index objects
 		self.indices = []
 		self.indexed_columns_names = set()
 		self.filename = filename
+		self.primary_key_col_name = None
+
+		#the file handle, Must be used within a function of table class
+		self.f = None
+		# self.f_updated = None #file handle is different for debugging purposes
 
 		self.initTableData()
+
+	# def __del__(self):
+	# 	self.f.close()
+	# 	self.f_updated.close()
 	def initTableData(self):
-		f = open(self.filename, 'r')
-		lines = f.readlines()
+		self.f = open(self.filename, 'rw+')
+		
+		# self.f_updated = open("./" + self.filename.split(".")[0] + "_updated.csv", 'rw+')
+		lines = self.f.readlines()
+
+		#create an array of line offsets, will be used in writing to the file (later)
+		#update this when a new record is inserted
+		self.line_lengths = [len(line) for line in lines]
 
 
 		#remove newline character
@@ -243,7 +338,12 @@ class Table:
 		self.header_line = lines[0]
 		# print header_line
 
+		#DEBUG#######################################
+		# self.f_updated.write(self.header_line + "\n")
+		#############################################
+
 		self.rows = list(filter(lambda line: line != '', lines[1:]))
+		self.rows_in_strings = self.rows
 		# print len(lines)
 
 
@@ -287,7 +387,7 @@ class Table:
 		# print hashed_index_on_col.keys()
 		
 		# print hashed_index_on_col[key]
-		index_obj = Index(colname, hashed_index_on_col)
+		index_obj = Index(colname, hashed_index_on_col, self)
 		self.indices.append(index_obj)
 		
 		# print self.getIndexOn(colname).getArray()
@@ -315,5 +415,53 @@ class Table:
 		return self.all_col_names
 	def getAllRows(self):
 		return self.rows
+	def setPrimaryKeyAs(self, col_name):
+		self.primary_key_col_name = col_name
+	def getPrimaryKeyCol(self):
+		return self.primary_key_col_name
+
+	def updateIndicesWith(self, row_string, row_index):
+		#update the index too
+		for index_obj in self.indices:
+
+			#find col_index to get the column value which serves as the key in the hashed array
+			col_index = self.getAllColNames().index(index_obj.getColName())
+			hash_key_col_value = ParseCSVString2Array(row_string)[col_index]
+			hash_val_row_index = row_index
+
+			#valid to use because the new row has been inserted already
+			all_row_indices = range(0,self.getRowCount())
+			_indices_that_need_increment = filter(lambda _row_index: _row_index >= row_index, all_row_indices)
+
+			index_obj.updateIndexWith(hash_key_col_value, row_index, _indices_that_need_increment)
+
+	#row_string has been stripped of newline character
+	def insertRowAt(self, row_string, row_index):
+		self.rows.insert(row_index, row_string)
+		self.updateIndicesWith(row_string, row_index)
+		self.writeRowsToFile(row_index)
+		
+		#important here because old line lengths need to be used first
+		self.line_lengths.insert(row_index, len(row_string + "\n"))
+
+	#only need to write the rows that start at the new_row_index
+	def writeRowsToFile(self, new_row_index):
+		
+		#by default rows don't have newline characters at the end, so it is added at the end of each line
+		nl_terminated_rows_in_strings = map(lambda row_string: row_string if row_string[-1] == "\n" else row_string + "\n", self.rows_in_strings[new_row_index:])
+		
+		#seek to the desired byte location
+		offset = sum(self.line_lengths[0:new_row_index + 1])
+		# print offset
+		# print self.line_leng	ths[0:new_row_index + 1]
+		self.f.seek(offset)
+		self.f.truncate()
+		# self.f_updated.truncate(offset)
+		
+		self.f.writelines(nl_terminated_rows_in_strings)
+		# self.f_updated.close()
+		
+		
+
 if __name__ == "__main__":
 	start()
--- Introduction to Databases/Assignments/1/test2.py
+++ Introduction to Databases/Assignments/1/test2.py
@@ -1,5 +1,18 @@
-import csv
+from threading import Thread
+import string
+from time import sleep
 
-f = open("customers.csv")
-reader = csv.reader(f, delimiter=",")
-print row[7]
+def printd(args):
+	for d in args:
+		print d
+		sleep(1)
+def printl(args):
+	for l in args:
+		print l;
+		sleep(1)
+
+thread1 = Thread(target = printd, args = (list(range(1,11)), ))
+thread2 = Thread(target = printl, args = (list(string.ascii_lowercase), ))
+
+thread1.start()
+thread2.start()
