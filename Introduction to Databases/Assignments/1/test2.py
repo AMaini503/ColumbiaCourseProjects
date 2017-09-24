a7d66c791778324fe52651acad47e54bd402b28b
AMaini503 <aayush.maini@gmail.com>
2017-09-24 16:31:10 -0400

    First commit for deep learning

--- Introduction to Databases/Assignments/1/test2.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern_for_constraint_query = re.compile(r"apply\s+unique\s+to\s+([A-za-z0-9]+)\.([A-za-z0-9]+)")
-
-while(True):
-	string = raw_input(">> ")
-	match = pattern_for_constraint_query.match(string)
-	if match:
-		print match.groups()
